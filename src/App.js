78a3f7982752157157ad04ff90f0db2eb887bbe1
Lucas Schaat <lucas.schaat@gmail.com>
2020-03-11 16:27:28 -0600

    Initialized app for Routing Morning Review

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './Components/Header'
-import routes from './routes'
 
 function App() {
   return (
     <div className="App">
-      <Header />;
-      {routes}
+
     </div>
   );
 }
